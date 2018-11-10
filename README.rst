e114d83cf2ed5ca6ffe2554f858a08dde064569c
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2018-11-10 14:54:53 -0500

    ENH: Improve CI.
    Improve the module's CI:
    - Add Travis and AppVeyor CI configuration `*.yml` files.
    - Rename and move the Circle CI `*.yml` file to the `.circleci` folder to
      conform to the `ITKModuleTemplate` skeleton.
    - Add the Python setup `setup.py` file
    - Add the Python wrapping `*.wrap` files.
    - Add the CI badges to the `README.rst` file.
    - Take advantage of the commit to improve the `README.rst` file.

--- README.rst
+++ README.rst
@@ -1,10 +1,40 @@
 ITKRLEImage
 ===========
 
-.. image:: https://circleci.com/gh/KitwareMedical/ITKRLEImage.svg?style=svg
-  :target: https://circleci.com/gh/KitwareMedical/ITKRLEImage
+.. |CircleCI| image:: https://circleci.com/gh/KitwareMedical/ITKRLEImage.svg?style=shield
+    :target: https://circleci.com/gh/KitwareMedical/ITKRLEImage
 
-Run-length encoded memory compression scheme for an `ITK <http://itk.org>`_ image.
+.. |TravisCI| image:: https://travis-ci.org/KitwareMedical/ITKRLEImage.svg?branch=master
+    :target: https://travis-ci.org/KitwareMedical/ITKRLEImage
+
+.. |AppVeyor| image:: https://img.shields.io/appveyor/ci/itkrobot/itkrleimage.svg
+    :target: https://ci.appveyor.com/project/itkrobot/itkrleimage
+
+=========== =========== ===========
+   Linux      macOS       Windows
+=========== =========== ===========
+|CircleCI|  |TravisCI|  |AppVeyor|
+=========== =========== ===========
+
+
+Overview
+--------
+
+Run-length encoded memory compression scheme for an `ITK <http://itk.org>`_
+image.
+
+The module provides run-length encoded storage for image content, iterators
+for efficient reading and writing, and a specialization of region of
+interest filter which can also be used to convert to and from regular
+`itk::Image`.
+
+
+License
+-------
+
+This software is distributed under the Apache 2.0 license. Please see
+the *LICENSE* file for details.
+
 
 Acknowledgements
 ----------------
---
+++ setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from os import sys
+
+try:
+    from skbuild import setup
+except ImportError:
+    print('scikit-build is required to build from source.', file=sys.stderr)
+    print('Please run:', file=sys.stderr)
+    print('', file=sys.stderr)
+    print('  python -m pip install scikit-build')
+    sys.exit(1)
+
+setup(
+    name='itk-rleimage',
+    version='1.0.0',
+    author='Paul A. Yushkevich',
+    author_email='pauly2@mail.med.upenn.edu',
+    packages=['itk'],
+    package_dir={'itk': 'itk'},
+    download_url=r'https://github.com/KitwareMedical/ITKRLEImage',
+    description=r'ITK classes for run-length encoded memory compression',
+    long_description='ITKRLEImage provides run-length encoded storage for'
+                     'image content, iterators for efficient reading and'
+                     'writing, and a specialization of region of interest'
+                     'filter which can also be used to convert to and from'
+                     'regular `itk::Image`.',
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: C++",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development :: Libraries",
+        "Operating System :: Android",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS"
+        ],
+    license='Apache',
+    keywords='ITK InsightToolkit Run-length-encoding',
+    url=r'https://github.com/KitwareMedical/ITKRLEImage',
+    install_requires=[
+        r'itk'
+    ]
+    )
